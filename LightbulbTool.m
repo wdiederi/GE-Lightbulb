e16a9e3c706ced17775b3aab8756c09619f0d8e4
wdiederi <wdiederi@nd.edu>
2017-04-18 21:01:07 -0400

    TESTING BECAUSE GIT IS DUMB

--- LightbulbTool.m
+++ LightbulbTool.m
@@ -1,6 +1,6 @@
 function varargout = LightbulbTool(varargin)
 
-%THIS IS A TEST.  4/18/17 @ 8:28
+% TESTING  4/18/17 @ 8:28
 % UPDATE LAST MODIFIED
 % modified April 18 2018 by Danny
 % Made the switch statment update the the string values for the edit text
